040d1f18ca68c5b7fde5ca9790fd49305aa7d404
sgbsc <44087056+sgbsc@users.noreply.github.com>
2018-10-19 14:50:30 +0100

    first commit

---
+++ src/main/java/com/sgbsc/SpringBootConfigurationValuesApplication.java
@@ -0,0 +1,12 @@
+package com.sgbsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigurationValuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfigurationValuesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sgbsc/SpringBootConfigurationValuesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sgbsc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootConfigurationValuesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
